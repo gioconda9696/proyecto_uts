b2288a13edf591f6b2400876ed0886332a5b791a
gioconda9696 <45276335+gioconda9696@users.noreply.github.com>
2024-06-07 14:49:38 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
+const app = express();
+const PORT = 2001;
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'wwmoni_uts',
+    password: 'Dios2024.',
+    database: 'wwmoni_uts'
+});
+
+connection.connect((error) => {
+    if (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        return;
+    }
+    console.log('Conexi�n exitosa a la base de datos');
+});
+
+app.use(cors({
+    origin: ['https://monitoreotelecoproyectouts.com', 'https://monitoreotelecoproyectouts.com:3000','http://monitoreotelecoproyectouts.com', 'http://monitoreotelecoproyectouts.com:3000'
+        ,'http://localhost:3000'
+        ,'http://192.168.105.215:3001'] // Ajusta seg�n sea necesario
+}));
+app.use(express.json());
+
+module.exports = { app, connection };
+require('./sfvo1.js');
+require('./sfvo2.js');
+
+// Ruta para verificar que el servidor est� funcionando
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando correctamente');
+});
+
+// Rutas API
+app.get('/usuarios', (req, res) => {
+    connection.query('SELECT * FROM users', (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).send('Error de servidor');
+        }
+        res.json(results);
+    });
+});
+
+app.post('/usuarios', (req, res) => {
+    const { user, password } = req.body;
+    connection.query('INSERT INTO users (user, password) VALUES (?, ?)', [user, password], (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).send('Error de servidor');
+        }
+        res.json({ message: 'Usuario creado correctamente' });
+    });
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    const { user, password } = req.body;
+    const userId = req.params.id;
+    connection.query('UPDATE users SET user=?, password=? WHERE Userid=?', [user, password, userId], (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).send('Error de servidor');
+        }
+        res.json({ message: 'Usuario actualizado correctamente' });
+    });
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    const userId = req.params.id;
+    connection.query('DELETE FROM users WHERE Userid=?', [userId], (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).send('Error de servidor');
+        }
+        res.json({ message: 'Usuario eliminado correctamente' });
+    });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    connection.query('SELECT * FROM users WHERE user = ? AND password = ?', [username, password], (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).json({ success: false, message: 'Error de servidor' });
+        }
+
+        if (results.length > 0) {
+            res.json({ success: true, message: 'Inicio de sesi�n exitoso' });
+        } else {
+            res.status(401).json({ success: false, message: 'Credenciales inv�lidas' });
+        }
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
---
+++ sfvo2.js
@@ -0,0 +1,229 @@
+const nodemailer = require('nodemailer');
+const ExcelJS = require('exceljs');
+const { app, connection } = require('./app.js');
+
+const NombreTabla = "sfvo2";
+const NombreExcel = "SFVO2";
+
+app.get('/sfvo2', (req, res) => {
+    connection.query(`SELECT Voltaje , Corriente , Hora FROM ${NombreTabla} ORDER BY Datoid DESC LIMIT 150`, (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).send('Error de servidor');
+        }
+        res.json(results);
+    });
+});
+
+app.get('/sfvo2_5min', (req, res) => {
+    connection.query(`SELECT * FROM ${NombreTabla} ORDER BY Datoid DESC LIMIT 1`, (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).send('Error de servidor');
+        }
+        res.json(results[0]);
+    });
+});
+
+app.get('/sfvo2_ultimosdatos', (req, res) => {
+    connection.query(`SELECT * FROM ${NombreTabla} ORDER BY Datoid DESC LIMIT 11`, (error, results, fields) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).send('Error de servidor');
+        }
+        const dataWithDifferences = results.map((data, index) => {
+            if (index === results.length - 1) {
+                return { ...data, VoltajeDiferencia: 0, CorrienteDiferencia: 0};
+            } else {
+                const voltajeDiferencia = data.Voltaje-results[index + 1].Voltaje;
+                const corrienteDiferencia = data.Corriente- results[index + 1].Corriente;
+                return { 
+                    ...data, 
+                    VoltajeDiferencia: voltajeDiferencia, 
+                    CorrienteDiferencia: corrienteDiferencia, 
+                };
+            }
+        });
+        res.json(dataWithDifferences);
+    });
+});
+
+app.get('/sfvo2_exportarexcel', (req, res) => {
+    // Consulta para obtener los datos de la tabla 'datos'
+    connection.query(`SELECT Voltaje , Corriente , Hora FROM ${NombreTabla}`, async (error, results) => {
+        if (error) {
+            console.error('Error al obtener los datos de la base de datos:', error);
+            res.status(500).send('Error al obtener los datos de la base de datos');
+            return;
+        }
+
+        try {
+            // Crear un nuevo archivo Excel
+            const workbook = new ExcelJS.Workbook();
+            const worksheet = workbook.addWorksheet(NombreExcel);
+
+            // Verificar si hay resultados de la consulta
+            if (results.length > 0) {
+                // Mapear los resultados para obtener solo los valores (filas)
+                const rows = results.map(result => Object.values(result));
+                
+                // Agregar los encabezados de las columnas
+                const columnHeaders = Object.keys(results[0]);
+                worksheet.addRow(columnHeaders);
+
+                // Agregar las filas de datos al archivo Excel
+                rows.forEach(row => {
+                    worksheet.addRow(row);
+                });
+
+                // Generar el archivo Excel en un buffer
+                const buffer = await workbook.xlsx.writeBuffer();
+
+                // Consulta para obtener los correos electrónicos de la tabla 'destinatarios'
+                connection.query('SELECT user FROM users', async (error, destinatarios) => {
+                    if (error) {
+                        console.error('Error al obtener los destinatarios de correo de la base de datos:', error);
+                        res.status(500).send('Error al obtener los destinatarios de correo de la base de datos');
+                        return;
+                    }
+
+                    try {
+                        if (destinatarios.length > 0) {
+                            // Construir la lista de destinatarios
+                            const destinatariosList = destinatarios.map(destinatario => destinatario.user).join(',');
+                            // Configurar nodemailer para enviar el correo electrónico
+                            const transporter = nodemailer.createTransport({
+                                service: 'Gmail',
+                                auth: {
+                                    user: 'proyectotelecomunicacionesuts@gmail.com',
+                                    pass: 'xxqe hzlo djkp jzsc'
+                                }
+                            });
+            
+                            // Adjuntar el archivo Excel al correo electrónico
+                            const mailOptions = {
+                                from: 'proyectotelecomunicacionesuts@gmail.com',
+                                to: destinatariosList,
+                                subject: 'Archivo Excel de datos actuales en el SFVO2',
+                                text: 'Se adjunta el archivo Excel con los datos actuales en el SFVO2.',
+                                attachments: [{
+                                    filename: `${NombreExcel}.xlsx`,
+                                    content: buffer
+                                }]
+                            };
+
+                            // Enviar el correo electrónico
+                            const info = await transporter.sendMail(mailOptions);
+                            console.log('Correo electrónico enviado: ' + info.response);
+                            res.send('Correo electrónico enviado correctamente');
+                        } else {
+                            console.error('No hay destinatarios de correo en la base de datos');
+                            res.status(500).send('No hay destinatarios de correo en la base de datos');
+                        }
+                    } catch (error) {
+                        console.error('Error al enviar el correo electrónico:', error);
+                        res.status(500).send('Error al enviar el correo electrónico');
+                    }
+                });
+            } else {
+                console.error('La consulta no devolvió resultados');
+                res.status(500).send('La consulta no devolvió resultados');
+            }
+        } catch (err) {
+            console.error('Error al procesar los datos:', err);
+            res.status(500).send('Error al procesar los datos');
+        }
+    });
+});
+
+// Función para exportar los datos y enviar el correo electrónico
+async function exportAndEmailData() {
+    
+    try {
+        const results = await queryDatabase(`SELECT Voltaje, Corriente, Hora FROM ${NombreTabla}`);
+        const destinatarios = await queryDatabase('SELECT user FROM users');
+
+        if (results.length > 0 && destinatarios.length > 0) {
+            // Crear el archivo Excel y configurar el transporte del correo
+            const workbook = new ExcelJS.Workbook();
+            const worksheet = workbook.addWorksheet(NombreExcel);
+            const rows = results.map(result => Object.values(result));
+            const columnHeaders = Object.keys(results[0]);
+            worksheet.addRow(columnHeaders);
+
+            rows.forEach(row => {
+                worksheet.addRow(row);
+            });
+
+            const buffer = await workbook.xlsx.writeBuffer();
+            const transporter = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                    user: 'proyectotelecomunicacionesuts@gmail.com',
+                    pass: 'xxqe hzlo djkp jzsc'
+                }
+            });
+
+            // Construir la lista de destinatarios
+            const destinatariosList = destinatarios.map(destinatario => destinatario.user).join(',');
+
+            // Configurar las opciones del correo electrónico
+            const mailOptions = {
+                from: 'proyectotelecomunicacionesuts@gmail.com',
+                to: destinatariosList,
+                subject: 'Base de Datos exportada de el SFVO2',
+                text: 'estos son los datos tomados dentro de los 15 dias de el SFVO2',
+                attachments: [{
+                    filename: `${NombreExcel}_15dias.xlsx`,
+                    content: buffer
+                }]
+            };
+
+            // Enviar el correo electrónico
+            const info = await transporter.sendMail(mailOptions);
+            console.log('Correo electrónico enviado: ' + info.response);
+            return;
+        } else {
+            console.error('La consulta no devolvió resultados o no hay destinatarios de correo.');
+            return;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return;
+    }
+}
+
+// Función para ejecutar una consulta a la base de datos
+function queryDatabase(sql) {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+async function performExecutionIfNeeded() {
+    const now = new Date();
+    if (now.getDate() === 15 || now.getDate() === 30) { // Si es el 15 o el 30
+    await exportAndEmailData(); // Ejecuta la tarea
+    }
+    if (now.getDate() === 30) {
+    // Borrar los datos de la tabla 'datos' después de enviar el correo
+    await queryDatabase(`TRUNCATE TABLE ${NombreTabla}`);
+    console.log('Datos borrados correctamente de la tabla.'); 
+    }
+    scheduleNextExecution(); // Programa la próxima ejecución
+}
+
+function scheduleNextExecution() {
+    const now = new Date();
+    const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1); // Obtén la fecha de mañana
+    const timeUntilTomorrow = tomorrow.getTime() - now.getTime(); // Calcula el tiempo hasta mañana
+    setTimeout(performExecutionIfNeeded, timeUntilTomorrow+60000); // Programa el temporizador para mañana
+}
+
+scheduleNextExecution(); // Programa la próxima ejecución
