6b5bc493e15d5b47169391e65913cb95c887795e
gioconda9696 <45276335+gioconda9696@users.noreply.github.com>
2024-06-10 11:52:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,106 +1,108 @@
 const express = require('express');
 const mysql = require('mysql');
-const cors = require('cors');
-const fs = require('fs');
+const cors = require('fs');
 const https = require('https');
 const app = express();
 const PORT = 2001;
-
-
-const connection = mysql.createConnection({
-    host: '162.240.158.67',
-    user: 'wwmoni_uts',
-    password: 'Dios2024.',
-    database: 'wwmoni_uts'
-});
-
-connection.connect((error) => {
-    if (error) {
-        console.error('Error al conectar a la base de datos:', error);
-        return;
-    }
-    console.log('Conexi�n exitosa a la base de datos');
-});
+const db = require('./db');
 
 app.use(cors({
     origin: ['https://monitoreotelecoproyectouts.com', 'https://monitoreotelecoproyectouts.com:3000','http://monitoreotelecoproyectouts.com', 'http://monitoreotelecoproyectouts.com:3000'
         ,'https://monitoreotelecoproyectouts.com/login'
         ,'https://www.monitoreotelecoproyectouts.com'
         ,'http://localhost:3000'
-        ,'http://192.168.105.215:3001'] // Ajusta seg�n sea necesario
+        ,'http://192.168.105.215:3001'] // Ajusta según sea necesario
 }));
 app.use(express.json());
 
-module.exports = { app, connection };
+module.exports = { app, db };
 require('./sfvo1.js');
 require('./sfvo2.js');
 
-// Ruta para verificar que el servidor est� funcionando
+// Ruta para verificar que el servidor está funcionando
 app.get('/', (req, res) => {
     res.send('Servidor funcionando correctamente');
 });
 
-// Rutas API
+// Ruta API para obtener usuarios
 app.get('/usuarios', (req, res) => {
-    connection.query('SELECT * FROM users', (error, results, fields) => {
+    const dbInstance = db.getInstance();
+    dbInstance.connect();
+    dbInstance.connection.query('SELECT * FROM users', (error, results, fields) => {
         if (error) {
             console.error('Error al ejecutar la consulta:', error);
             return res.status(500).send('Error de servidor');
         }
         res.json(results);
+        dbInstance.disconnect();
     });
 });
 
+// Ruta API para crear un usuario
 app.post('/usuarios', (req, res) => {
     const { user, password } = req.body;
-    connection.query('INSERT INTO users (user, password) VALUES (?, ?)', [user, password], (error, results, fields) => {
+    const dbInstance = db.getInstance();
+    dbInstance.connect();
+    dbInstance.connection.query('INSERT INTO users (user, password) VALUES (?, ?)', [user, password], (error, results, fields) => {
         if (error) {
             console.error('Error al ejecutar la consulta:', error);
             return res.status(500).send('Error de servidor');
         }
         res.json({ message: 'Usuario creado correctamente' });
+        dbInstance.disconnect();
     });
 });
 
+// Ruta API para actualizar un usuario
 app.put('/usuarios/:id', (req, res) => {
     const { user, password } = req.body;
     const userId = req.params.id;
-    connection.query('UPDATE users SET user=?, password=? WHERE Userid=?', [user, password, userId], (error, results, fields) => {
+    const dbInstance = db.getInstance();
+    dbInstance.connect();
+    dbInstance.connection.query('UPDATE users SET user=?, password=? WHERE Userid=?', [user, password, userId], (error, results, fields) => {
         if (error) {
             console.error('Error al ejecutar la consulta:', error);
             return res.status(500).send('Error de servidor');
         }
         res.json({ message: 'Usuario actualizado correctamente' });
+        dbInstance.disconnect();
     });
 });
 
+// Ruta API para eliminar un usuario
 app.delete('/usuarios/:id', (req, res) => {
     const userId = req.params.id;
-    connection.query('DELETE FROM users WHERE Userid=?', [userId], (error, results, fields) => {
+    const dbInstance = db.getInstance();
+    dbInstance.connect();
+    dbInstance.connection.query('DELETE FROM users WHERE Userid=?', [userId], (error, results, fields) => {
         if (error) {
             console.error('Error al ejecutar la consulta:', error);
             return res.status(500).send('Error de servidor');
         }
         res.json({ message: 'Usuario eliminado correctamente' });
+        dbInstance.disconnect();
     });
 });
 
+// Ruta API para iniciar sesión
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    connection.query('SELECT * FROM users WHERE user = ? AND password = ?', [username, password], (error, results, fields) => {
+    const dbInstance = db.getInstance();
+    dbInstance.connect();
+    dbInstance.connection.query('SELECT * FROM users WHERE user = ? AND password = ?', [username, password], (error, results, fields) => {
         if (error) {
             console.error('Error al ejecutar la consulta:', error);
             return res.status(500).json({ success: false, message: 'Error de servidor' });
         }
 
         if (results.length > 0) {
-            res.json({ success: true, message: 'Inicio de sesi�n exitoso' });
+            res.json({ success: true, message: 'Inicio de sesión exitoso' });
         } else {
-            res.status(401).json({ success: false, message: 'Credenciales inv�lidas' });
+            res.status(401).json({ success: false, message: 'Credenciales inválidas' });
         }
+        dbInstance.disconnect();
     });
 });
-
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);
